c192190aa4af72c3a09e57826807d1ef53d23b66
franjpr <franpr90@gmail.com>
2021-11-07 21:55:33 +0000

    feat: configurar un theme

--- src/Layout/Footer/Footer.styled.ts
+++ src/Layout/Footer/Footer.styled.ts
@@ -1,9 +1,16 @@
 import styled from 'styled-components';
+import { CustomTheme } from '../../../styles/theme/theme.model';
+
+interface Props {
+  theme: CustomTheme;
+}
 
 export const StyledFooter = styled.footer`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 5vh;
-  background-color: aliceblue;
+  background-color: ${({ theme }: Props) => theme.colors.primary};
+  color: ${({ theme }: Props) => theme.colors.foregroundDark};
+  font-size: ${({ theme }: Props) => theme.fonts.mobile.body.size};
 `;
