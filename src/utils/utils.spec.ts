2a844cdb5a312ba7814124fe17d4da8a93e6e2c1
franjpr <franpr90@gmail.com>
2021-11-09 22:19:37 +0000

    feat: implementado utilidades para formatear costes & fechas

---
+++ src/utils/utils.spec.ts
@@ -0,0 +1,24 @@
+import { construirBeneficiario } from '../Models/contratos.model';
+import { calcularTotal, formatearCoste, formatearFecha } from './utils';
+
+describe('Utils Specs', () => {
+  it('Formatea un dígito añadiendole la moneda y separadores correctos', () => {
+    expect(formatearCoste(100)).toEqual('100,00 €');
+    expect(formatearCoste(10000)).toEqual('10.000,00 €');
+    expect(formatearCoste(-1)).toEqual('-1,00 €');
+  });
+
+  it('Formatea una fecha en ISO a formato corto aaaa-mm-dd', () => {
+    expect(formatearFecha('2013-08-03T02:00:00Z')).toEqual('2013-08-03');
+    expect(formatearFecha('2013/08/03')).toEqual('2013-08-03');
+    expect(formatearFecha('2013-08-03')).toEqual('2013-08-03');
+    expect(formatearFecha('20-08-2003')).toEqual('-');
+  });
+
+  it('Calcula el total de costes entre los beneficiarios', () => {
+    const beneficiarios1 = [construirBeneficiario({ coste: 10 }), construirBeneficiario({}), construirBeneficiario({ coste: 10 })];
+    const beneficiarios2 = [construirBeneficiario({ coste: 10 }), construirBeneficiario({ coste: 10 })];
+    expect(calcularTotal(beneficiarios1)).toEqual(20);
+    expect(calcularTotal(beneficiarios2)).toEqual(20);
+  });
+});
