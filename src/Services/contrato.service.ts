46e2cbeb038bda0065a0599253b7aef908fe0292
franjpr <franpr90@gmail.com>
2021-11-11 17:33:43 +0000

    fix: fix en el nombre de la variable de entorno

--- src/Services/contrato.service.ts
+++ src/Services/contrato.service.ts
@@ -3,7 +3,7 @@ import { Contrato } from '../Models/contratos.model';
 const getContratos = (fechaInicio: string, fechaFin: string = ''): Promise<Contrato[]> => {
   const fechaFinal = fechaFin ? `&fechaFin=${fechaFin}` : '';
   const query = `?fechaInicio=${fechaInicio}${fechaFinal}`;
-  const url = `https://boe-backend.vercel.app/contratos/rango${query}`;
+  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/contratos/rango${query}`;
 
   return fetch(url, { method: 'get' })
     .then((response) => response.json())
