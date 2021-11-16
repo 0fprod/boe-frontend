6be5003c7c60fe648916219938c6d3e877345dd5
franjpr <franpr90@gmail.com>
2021-11-16 00:06:11 +0000

    feat: calculos de costes por actividad

--- src/utils/calculos.estadisticas.ts
+++ src/utils/calculos.estadisticas.ts
@@ -6,7 +6,7 @@ export const costeSegunTipoDeEmpresa = (beneficiario: Beneficiario[], pyme: bool
   if (pyme) {
     costeTotal = beneficiario.reduce((costeAcumulado, { esPyme, coste }) => (esPyme ? (costeAcumulado += coste) : costeAcumulado), 0);
   } else {
-    costeTotal = beneficiario.reduce((costeAcumulado, { esPyme, coste }) => (esPyme ? (costeAcumulado += costeAcumulado) : coste), 0);
+    costeTotal = beneficiario.reduce((costeAcumulado, { esPyme, coste }) => (esPyme ? (costeAcumulado += coste) : coste), 0);
   }
   return costeTotal;
 };
@@ -21,9 +21,9 @@ export const totalContratosPorPymes = (contratos: Contrato[]): DatosGrafico => {
     return costeAcumulado;
   }, 0);
   return [
-    ['PYMES/NO PYMES', 'Ingresos totales'],
+    ['PYMES/Grandes Empresas', 'Ingresos totales'],
     ['PYMES', totalCostePymes],
-    ['NO PYMES', totalCosteNoPymes],
+    ['Grandes Empresas', totalCosteNoPymes],
   ];
 };
 
@@ -31,7 +31,27 @@ export const gastoTotal = (datos: DatosGrafico) => {
   const [titulo, ...resto] = datos;
 
   return resto.reduce((costeAcumulado, [_titulo, coste]) => {
-    if (Number.isInteger(coste) && coste >= 0) return (costeAcumulado += coste);
+    if (!Number.isNaN(coste) && coste >= 0) return (costeAcumulado += coste);
     return costeAcumulado;
   }, 0);
+};
+
+const gastoTotalDeBeneficiarios = (beneficiarios: Beneficiario[]): number =>
+  beneficiarios.reduce((costeAcumulado, { coste }) => (costeAcumulado += coste), 0);
+
+export const totalContratosPorActividad = (contratos: Contrato[]): DatosGrafico => {
+  const datosPorActividad = new Map<string, number>();
+
+  contratos.forEach(({ detalles: { institucion, beneficiarios } }) => {
+    const actividad = institucion.actividad;
+    const total = gastoTotalDeBeneficiarios(beneficiarios);
+    if (!actividad) {
+      const acumuladoSinActividad = datosPorActividad.get('Sin actividad definida') ?? 0;
+      datosPorActividad.set('Sin actividad definida', acumuladoSinActividad + total);
+    } else {
+      datosPorActividad.set(actividad, total);
+    }
+  });
+
+  return [['Actividad', 'Coste por actividad'], ...Array.from(datosPorActividad)];
 };
