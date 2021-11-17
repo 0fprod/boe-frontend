ae1e9e14f6d7314fdb32dc979062b6a72f6638d7
franjpr <franpr90@gmail.com>
2021-11-17 12:37:12 +0000

    style: ajustado el texto responsive de la pagina de informacion

--- src/Components/Info/Informacion.tsx
+++ src/Components/Info/Informacion.tsx
@@ -12,29 +12,29 @@ export const Informacion: React.FC<{}> = () => {
       </p>
       <p>
         En esta web no se guarda ningún dato del usuario visitante, y además se compromete a respetar las reglas de reutilización definidas
-        en este enlace{' '}
+        en este{' '}
         <a rel="noreferrer" href="https://www.boe.es/informacion/aviso_legal/index.php#reutilizacion" target="_blank">
-          https://www.boe.es/informacion/aviso_legal/index.php#reutilizacion
+          enlace
         </a>
         .
       </p>
       <p>Funcionamiento:</p>
       <ul>
         <li>
-          - Cada día a las 7:00 UTC se ataca a los datos abiertos que proporciona la web{' '}
+          Cada día a las 7:00 UTC se ataca a los datos abiertos que proporciona la{' '}
           <a rel="noreferrer" href="https://www.boe.es/datosabiertos/">
-            https://www.boe.es/datosabiertos/
+            web
           </a>
           .
         </li>
-        <li> - Se extraen las formalizaciones de contratos en formato XML.</li>
-        <li> - Dicha información se organiza y se guarda en una base de datos en formato JSON. </li>
-        <li> - Esta web obtiene la información de la base de datos para graficarla.</li>
+        <li> Se extraen las formalizaciones de contratos en formato XML.</li>
+        <li> Dicha información se organiza y se guarda en una base de datos en formato JSON. </li>
+        <li> Esta web obtiene la información de la base de datos para graficarla.</li>
       </ul>
       <p>
-        El código del proyecto lo pueden ver en github, si quieren colaborar o tienes alguna idea estos son los repositorios del{' '}
-        <a href="https://github.com/franjpr/boe-backend">servidor</a> y del <a href="https://github.com/franjpr/boe-frontend">cliente</a>{' '}
-        por si quieres abrir alguna pull-request o lo que sea.
+        El código del proyecto lo pueden ver en github, si alguien quiere colaborar o tiene alguna idea para implementar... estos son los
+        repositorios del <a href="https://github.com/franjpr/boe-backend">servidor</a> y del{' '}
+        <a href="https://github.com/franjpr/boe-frontend">cliente</a> por si quieres abrir alguna pull-request o lo que sea.
       </p>
     </InformacionWrapper>
   );
