0fa5ebce2bfbbc5f95201823844a732cafca54e1
franjpr <franpr90@gmail.com>
2021-11-25 12:12:45 +0000

    refactor: codigo defensivo en el mapper de estadisticas

--- src/Services/estadistica.mapper.ts
+++ src/Services/estadistica.mapper.ts
@@ -2,23 +2,40 @@ import { EstadisticasApiModel } from '../Models/api.model';
 import { DatosGrafico } from '../Models/datos.model';
 import { COLORES_GRAFICOS } from '../utils/utils';
 
-export const mapEstadisticasApiDatosGrafico = (estadisticas: EstadisticasApiModel[], label?: string): DatosGrafico => ({
-  labels: estadisticas.map(({ etiqueta }) => etiqueta),
-  datasets: [
-    {
-      data: estadisticas.map(({ valor }) => valor),
-      backgroundColor: COLORES_GRAFICOS.slice(0, estadisticas.length),
-    },
-  ],
-});
+export const mapEstadisticasApiDatosGrafico = (estadisticas: EstadisticasApiModel[]): DatosGrafico => {
+  if (!estadisticas || !Array.isArray(estadisticas)) {
+    return {
+      labels: [],
+      datasets: [],
+    };
+  }
+  return {
+    labels: estadisticas.map(({ etiqueta }) => etiqueta),
+    datasets: [
+      {
+        data: estadisticas.map(({ valor }) => valor),
+        backgroundColor: COLORES_GRAFICOS.slice(0, estadisticas.length),
+      },
+    ],
+  };
+};
 
-export const mapEstadisticasApiDatosGraficoBarras = (estadisticas: EstadisticasApiModel[], label: string): DatosGrafico => ({
-  labels: estadisticas.map(({ etiqueta }) => etiqueta),
-  datasets: [
-    {
-      label,
-      data: estadisticas.map(({ valor }) => valor),
-      backgroundColor: COLORES_GRAFICOS.slice(0, estadisticas.length),
-    },
-  ],
-});
+export const mapEstadisticasApiDatosGraficoBarras = (estadisticas: EstadisticasApiModel[], label: string): DatosGrafico => {
+  if (!estadisticas || !Array.isArray(estadisticas)) {
+    return {
+      labels: [],
+      datasets: [],
+    };
+  }
+
+  return {
+    labels: estadisticas.map(({ etiqueta }) => etiqueta),
+    datasets: [
+      {
+        label,
+        data: estadisticas.map(({ valor }) => valor),
+        backgroundColor: COLORES_GRAFICOS.slice(0, estadisticas.length),
+      },
+    ],
+  };
+};
