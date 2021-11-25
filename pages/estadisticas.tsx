0dbefa458cf06f9d8b5fb4d923cef05ea07475e2
franjpr <franpr90@gmail.com>
2021-11-25 11:49:28 +0000

    feat: implementada la pagina de estadisticas

--- pages/estadisticas.tsx
+++ pages/estadisticas.tsx
@@ -2,51 +2,69 @@ import { NextPage } from 'next';
 import React, { useEffect, useState } from 'react';
 import { Graficas } from '../src/Components/Graficas/Graficas';
 import { Layout } from '../src/Layout/Layout';
-import { Contrato } from '../src/Models/contratos.model';
-import { ContratoService } from '../src/Services/contrato.service';
+import { DatosGrafico } from '../src/Models/datos.model';
+import { EstadisticasService } from '../src/Services/estadisticas.service';
 import { formatearFecha } from '../src/utils/utils';
 
 interface Props {
-  contratos: Contrato[];
+  estadisticasPymes: DatosGrafico;
+  estadisticasActividad: DatosGrafico;
+  estadisticasBeneficiarios: DatosGrafico;
+  obtenerContratosPorFecha: (fechaInicio: string, fechaFin?: string) => void;
 }
 
-const Estadisticas: NextPage<Props> = ({ contratos }) => {
-  const [listaContratos, setListaContratos] = useState(contratos);
+const Estadisticas: NextPage<Props> = ({ estadisticasBeneficiarios, estadisticasPymes, estadisticasActividad }) => {
+  const [pymesStats, setPymesStats] = useState(estadisticasPymes);
+  const [activityStats, setActivityStats] = useState(estadisticasActividad);
+  const [beneficiarioStats, setBeneficiarioStats] = useState(estadisticasBeneficiarios);
   const [cargando, setCargando] = useState(true);
 
   const handleContrato = async (fechaInicio: string, fechaFin?: string) => {
     try {
       setCargando(true);
-      const contratos: Contrato[] = await ContratoService.getContratos(fechaInicio, fechaFin);
-      setListaContratos(contratos);
+      const estadisticasPymes: DatosGrafico = await EstadisticasService.estadisticasPymes(fechaInicio, fechaFin);
+      const estadisticasActividad: DatosGrafico = await EstadisticasService.estadisticasActividad(fechaInicio, fechaFin);
+      const estadisticasBeneficiarios: DatosGrafico = await EstadisticasService.estadisticasTop10Beneficiarios(fechaInicio, fechaFin);
+      setPymesStats(estadisticasPymes);
+      setActivityStats(estadisticasActividad);
+      setBeneficiarioStats(estadisticasBeneficiarios);
     } catch (error) {
-      setListaContratos([]);
+      setPymesStats({ labels: [], datasets: [] });
+      setActivityStats({ labels: [], datasets: [] });
     }
   };
 
   useEffect(() => {
     setCargando(false);
-  }, [listaContratos]);
+  }, [pymesStats]);
 
   return (
     <Layout>
-      <Graficas contratos={listaContratos} obtenerContratosPorFecha={handleContrato} cargando={cargando}></Graficas>
+      <Graficas
+        estadisticasPymes={pymesStats}
+        estadisticasActividad={activityStats}
+        estadisticasBeneficiarios={beneficiarioStats}
+        obtenerContratosPorFecha={handleContrato}
+        cargando={cargando}
+      />
     </Layout>
   );
 };
 
 export async function getStaticProps() {
-  let contratos;
-  const hoy = formatearFecha(new Date().toISOString());
-  try {
-    contratos = await ContratoService.getContratos(hoy);
-  } catch (err) {
-    console.error('Err --> ', err);
-  }
+  const dHoy = new Date();
+  const hoy = formatearFecha(new Date(dHoy.getTime()).toISOString());
+  const manana = formatearFecha(new Date(dHoy.getFullYear(), dHoy.getMonth(), dHoy.getDate() + 1).toISOString());
+
+  const estadisticasPymes: DatosGrafico = await EstadisticasService.estadisticasPymes(hoy, manana);
+  const estadisticasActividad: DatosGrafico = await EstadisticasService.estadisticasActividad(hoy, manana);
+  const estadisticasBeneficiarios: DatosGrafico = await EstadisticasService.estadisticasTop10Beneficiarios(hoy, manana);
 
   return {
     props: {
-      contratos,
+      estadisticasPymes,
+      estadisticasActividad,
+      estadisticasBeneficiarios,
     },
   };
 }
