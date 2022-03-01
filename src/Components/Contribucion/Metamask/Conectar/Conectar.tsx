d83ef86d8b991d78a026ee8f956d8ac1077ceae9
Fran Palacios <franp.dev@protonmail.com>
2022-03-01 10:25:22 +0000

    :white_check_mark: implementado el btn de conectar

---
+++ src/Components/Contribucion/Metamask/Conectar/Conectar.tsx
@@ -0,0 +1,40 @@
+import { Web3ReactHooks } from '@web3-react/core';
+import { Red, REDES } from '../../../../../redes';
+import { SelectorDeRed } from './SelectorDeCadena';
+
+interface Props {
+  conectar: (idRed: number) => void;
+  desconectar: () => void;
+  isActivating: ReturnType<Web3ReactHooks['useIsActivating']>;
+  isActive: ReturnType<Web3ReactHooks['useIsActive']>;
+}
+
+export const Conectar = (props: Props) => {
+  const { conectar, desconectar, isActive, isActivating } = props;
+  const entornoDeRedPorDefecto = process.env.NEXT_PUBLIC_CHAIN_ENV === 'testnet' ? 'testnet' : 'mainnet';
+
+  const connect = () => {
+    conectar(REDES[entornoDeRedPorDefecto][0].chainId);
+  };
+
+  const cambioDeRed = (chain: Red) => {
+    conectar(chain.chainId);
+  };
+
+  if (isActive && !isActivating) {
+    return (
+      <>
+        <SelectorDeRed {...{ cambioDeRed, entornoDeRedPorDefecto }} />
+        <button onClick={desconectar}>Desconectar</button>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <button onClick={connect} disabled={isActivating}>
+        Conectar
+      </button>
+    </>
+  );
+};
