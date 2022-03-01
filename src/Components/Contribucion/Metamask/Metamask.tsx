90c4fe395144eaa536be1cedffa90703b64addd4
Fran Palacios <franp.dev@protonmail.com>
2022-03-01 10:25:44 +0000

    :sparkles: implementado el componente de interaccion con metamask

---
+++ src/Components/Contribucion/Metamask/Metamask.tsx
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+import { hooks, metaMask } from '../../../Conectores/metamask';
+import { Conectar } from './Conectar/Conectar';
+import { Estado } from './Conectar/Estado';
+
+const { useIsActive, useIsActivating, useError, useChainId } = hooks;
+
+export const Metamask = () => {
+  const isActivating = useIsActivating();
+  const isActive = useIsActive();
+  const error = useError();
+  const chainId = useChainId();
+
+  useEffect(() => {
+    // Se conecta al metamask, a la red que tenga permiso de conexion concedido a este dominio
+    metaMask.connectEagerly();
+  }, []);
+
+  const conectar = (idRed: number) => {
+    metaMask.activate(idRed);
+  };
+
+  const desconectar = () => {
+    metaMask.deactivate();
+  };
+
+  return (
+    <>
+      <>
+        <Estado {...{ isActive, isActivating, error, chainId }} />
+        <br />
+        <Conectar isActivating={isActivating} isActive={isActive} conectar={conectar} desconectar={desconectar} />
+      </>
+    </>
+  );
+};
